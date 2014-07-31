dec9fefd442d7f26f6fbe2ad5c2bb0dd5cf84644
Mark Hindess <mark.hindess@gmail.com>
2014-07-31 11:23:47 +0100

    More tests for server.

--- test/red/server_spec.js
+++ test/red/server_spec.js
@@ -14,9 +14,119 @@
  * limitations under the License.
  **/
 var should = require("should");
+var sinon = require('sinon');
+var request = require('supertest');
+var http = require('http');
+var express = require('express');
 
-describe("red/server", function() {
-    it('can be required without errors', function() {
-        require("../../red/server");
+var util = require('util');
+var fs = require('fs-extra');
+var path = require('path');
+var when = require('when');
+
+var app = express();
+var RED = require("../../red/red.js");
+var server = require("../../red/server.js");
+var nodes = require("../../red/nodes");
+
+describe("library", function() {
+    var userDir = path.join(__dirname,".testUserHome");
+    before(function(done) {
+        fs.remove(userDir,function(err) {
+            fs.mkdir(userDir,function() {
+                sinon.stub(nodes, 'load', function() {
+                    return when.promise(function(resolve,reject){
+                        resolve([]);
+                    });
+                });
+                RED.init(http.createServer(function(req,res){app(req,res)}),
+                         {userDir: userDir});
+                server.start().then(function () { done(); });
+            });
+        });
     });
+
+    after(function(done) {
+        fs.remove(userDir,done);
+        server.stop();
+        nodes.load.restore();
+    });
+
+    afterEach(function(done) {
+        fs.remove(userDir,function(err) {
+            fs.mkdir(userDir,done);
+        });
+    });
+
+    it('empty node configuration', function(done) {
+        request(RED.httpAdmin)
+            .get('/nodes')
+            .expect(200)
+            .end(function(err,res) {
+                if (err) {
+                    throw err;
+                    }
+                res.text.should.equal('<script type="text/javascript"></script>');
+                done();
+            });
+    });
+
+    it('empty list of flows', function(done) {
+        request(RED.httpAdmin)
+            .get('/flows')
+            .expect(200)
+            .end(function(err,res) {
+                if (err) {
+                    throw err;
+                }
+                res.body.should.eql([]);
+                done();
+            });
+    });
+
+    it('can update and read flows', function(done) {
+        var flow = [{"id":"n1","type":"test"}];
+        request(RED.httpAdmin)
+            .post('/flows')
+            .set('Content-Type', 'application/json; charset=UTF-8')
+            .send(JSON.stringify(flow))
+            .expect(204).end(function (err, res) {
+                if (err) {
+                    throw err;
+                }
+                request(RED.httpAdmin)
+                    .get('/flows')
+                    .expect(200)
+                    .end(function(err,res) {
+                        if (err) {
+                            throw err;
+                        }
+                        res.body.should.eql(flow);
+                        done();
+                    });
+            });
+    });
+
+    describe("start", function() {
+        it("handles node loading errors", function (done) {
+            server.stop();
+            nodes.load.restore();
+            sinon.stub(nodes, 'load', function() {
+                return when.promise(function(resolve,reject){
+                    resolve(['node load failed']);
+                });
+            });
+            var log = '';
+            sinon.stub(util, 'log', function(message) {
+                log += message + "\n";
+            });
+            server.start().then(function () {
+                log.should.containEql('Failed to register 1 node type');
+                server.stop();
+                util.log.restore();
+                done();
+            });
+        });
+    });
+
 });
